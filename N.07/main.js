39a56357b6c5de1b26fac07c6e40845e65e1a7a1
igorburak <biv992@gmail.com>
2019-07-14 00:17:06 +0300

    n.07

---
+++ N.07/main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let yourStr = prompt('Введите строку: ');
+
+    console.time("time");
+    function vowelsSum1(str) {
+        let vowelsArray = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+        let strToArray = str.split('');
+        let counter = 0;
+
+        for(let i = 0; i < vowelsArray.length; i++){
+            for(let n = 0; n < strToArray.length; n++){
+                if (strToArray[n] === vowelsArray[i]){
+                    counter++
+                }
+            }
+        }
+        return counter
+    }
+    console.timeEnd('time');
+
+
+    console.time('time2');
+    const vowelsSum2 = (s) => s.split('').filter(c => 'аоиеёэыуюя'.includes(c)).length;
+    console.timeEnd('time2');
+
+
+
+    vowelsSum1(yourStr);
+    vowelsSum2(yourStr);
+    console.log('Количество гласных -', vowelsSum1(yourStr))
+});
