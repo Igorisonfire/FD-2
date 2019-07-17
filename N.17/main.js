31ce75d37908b57c2001a6fb974d177717158ac1
igor.burak <igor.burak@zensoft.io>
2019-07-17 12:42:57 +0300

    forEach

--- N.17/main.js
+++ N.17/main.js
@@ -2,12 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let yourStr = prompt('Введите строку: ');
 
+    console.time('time1');
+    const vowelsSumFilter = (str) => str.split('').filter(item => 'аоиеёэыуюя'.includes(item)).length;
+    console.timeEnd('time1');
 
     console.time('time2');
-    const vowelsSumFilter = (str) => str.split('').filter(c => 'аоиеёэыуюя'.includes(c)).length;
+    const vowelsSumForEach = (str) => {
+        let counter = 0;
+        str.split('').forEach((itemText) => {
+            let vowelsArray = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+
+            vowelsArray.forEach((itemVowels) =>{
+                if (itemText === itemVowels){
+                    counter++
+                }
+            });
+        });
+        return counter
+    };
     console.timeEnd('time2');
 
 
     vowelsSumFilter(yourStr);
-    console.log('Количество гласных -', vowelsSumFilter(yourStr))
+    console.log('Количество гласных -', vowelsSumFilter(yourStr));
+
+    vowelsSumForEach(yourStr);
+    console.log('Количество гласных -', vowelsSumForEach(yourStr));
 });
