6de4a6fbd8ff91e90b20401bd295cb93de5c4f00
igorburak <biv992@gmail.com>
2019-07-18 01:00:58 +0300

    n.17

--- N.17/main.js
+++ N.17/main.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     return sum
                 }
             });
-        }, 0);
+        });
     };
     console.timeEnd('time3');
 
