4b27d30d81c0a4dec82ee84c59bd1f05ea6a6486
igorburak <biv992@gmail.com>
2021-01-15 22:39:12 +0100

    new2

--- 2-N.05/main.js
+++ 2-N.05/main.js
@@ -66,7 +66,64 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-    console.log('final = ', finalFunc(16))
+    // площать перекрытия прямоугольников
+
+    function getRect(coordinates) {
+        let x1, x2, y1, y2
+        let xArray = []
+        let yArray = []
+
+        coordinates.forEach((item, i) => {
+            if (i % 2 === 0) {
+                xArray.push(item)
+            } else {
+                yArray.push(item)
+            }
+        })
+
+        x1 = Math.min(...xArray)
+        x2 = Math.max(...xArray)
+        y1 = Math.min(...yArray)
+        y2 = Math.max(...yArray)
+
+        return {x1, x2, y1, y2}
+    }
+
+    function ArrayChallenge(strArr) {
+        let numbersArr = []
+
+        strArr[0].replace(/[\(\)]/g,'').split(",").forEach((item) => {
+            if(Number.isInteger(Number(item))) {
+                numbersArr.push(Number(item))
+            }
+        })
+
+        const firstRectCoordinates = numbersArr.slice(0,8)
+        const secondRectCoordinates = numbersArr.slice(8,17)
+
+        const firsRect = getRect(firstRectCoordinates)
+        const secondRect = getRect(secondRectCoordinates)
+
+        const left = Math.max(firsRect.x1, secondRect.x1);
+        const top = Math.min(firsRect.y2, secondRect.y2);
+        const right = Math.min(firsRect.x2, secondRect.x2);
+        const bottom = Math.max(firsRect.y1, secondRect.y1);
+
+        const width = right - left;
+        const height = top - bottom;
+
+        const overlapArea = width * height
+
+        const firsRectArea = (firsRect.x2 - firsRect.x1) * (firsRect.y2 - firsRect.y1)
+
+        if (width === 0 || height === 0) {
+            return 0
+        } else return Math.floor(firsRectArea / overlapArea)
+    }
+
+
+
+    console.log('final = ', ArrayChallenge(["(0,0),(5,0),(0,2),(5,2),(2,1),(5,1),(2,-1),(5,-1)"]))
 });
 
 
